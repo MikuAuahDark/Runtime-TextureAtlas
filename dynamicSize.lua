b2193faff190c59914008c77f361c0a7eb887c2e
Miku AuahDark <lumi.hotaru@gmail.com>
2022-01-30 12:43:07 +0800

    Restore original treeNode algorithm.
    The new grid algorithm gives us issue.

--- dynamicSize.lua
+++ dynamicSize.lua
@@ -8,13 +8,21 @@ dynamicSizeTA.__index = dynamicSizeTA
 
 -- Custom algorithm, see the file for more information
 local grid = require(path .. "packing")
+local treeNode = require(path .. "treeNode")
 
 local lg = love.graphics
 local newImageData = love.image.newImageData
 local sort = table.sort
 
-dynamicSizeTA.new = function(padding, extrude, spacing)
-  return setmetatable(baseAtlas.new(padding, extrude, spacing), dynamicSizeTA)
+local algoList = {
+  grid = grid,
+  tree = treeNode
+}
+
+dynamicSizeTA.new = function(padding, extrude, spacing, algorithm)
+  local t = setmetatable(baseAtlas.new(padding, extrude, spacing), dynamicSizeTA)
+  t.algorithm = assert(algoList[algorithm or "grid"], "invalid algorithm")
+  return t
 end
 
 local area = function(a, b)
@@ -48,33 +56,49 @@ dynamicSizeTA.bake = function(self, sortBy)
     end
 
     -- Calculate positions and size of canvas
-    local grid = grid.new(self.maxWidth or self._maxCanvasSize, self.maxHeight or self._maxCanvasSize)
+    local maxWidth, maxHeight = 0, 0
+    local packer = self.algorithm.new(self.maxWidth or self._maxCanvasSize, self.maxHeight or self._maxCanvasSize)
 
     for _, image in ipairs(shallowCopy) do
       local img = image.image
       local width, height = util.getImageDimensions(img)
       width = width + self.spacing + self.extrude * 2 + self.padding * 2
       height = height + self.spacing + self.extrude * 2 + self.padding * 2
-      grid:insert(width, height, image) -- will always be successful or will error
+      local node = packer:insert(width, height, image) -- will always be successful or will error
+      if self.algorithm == treeNode then
+        if node then
+          if node.x + width > maxWidth then
+            maxWidth = node.x + width
+          end
+          if node.y + height > maxHeight then
+            maxHeight = node.y + height
+          end
+        else
+          error("Could not fit image inside tree. Image ID: "..image.id)
+        end
+      end
     end
-    local maxWidth, maxHeight = grid.currentWidth - self.spacing, grid.currentHeight - self.spacing
-    
+
+    if self.algorithm == grid then
+      maxWidth, maxHeight = packer.currentWidth - self.spacing, packer.currentHeight - self.spacing
+    end
+
     if self.bakeAsPow2 then
       maxWidth = math.pow(2, math.ceil(math.log(maxWidth)/math.log(2)))
       maxHeight = math.pow(2, math.ceil(math.log(maxHeight)/math.log(2)))
     end
-    
+
     local data
     if self._pureImageMode then
       data = newImageData(maxWidth, maxHeight, "rgba8")
-      grid:draw(self.quads, maxWidth, maxHeight, self.extrude, self.padding, data)
+      packer:draw(self.quads, maxWidth, maxHeight, self.extrude, self.padding, data)
       self.image = data
     else
       local canvas = lg.newCanvas(maxWidth, maxHeight, self._canvasSettings)
       lg.push("all")
       lg.setBlendMode("replace")
       lg.setCanvas(canvas)
-      grid:draw(self.quads, maxWidth, maxHeight, self.extrude, self.padding)
+      packer:draw(self.quads, maxWidth, maxHeight, self.extrude, self.padding)
       lg.pop()
       data = canvas:newImageData()
       self.image = lg.newImage(data)
